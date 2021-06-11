294c30657b837364ced1e293b06f1cb3b5b6a683
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-11 11:43:44 -0500

    Create User schema

--- server/app.js
+++ server/app.js
@@ -2,13 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
+const { MONGOURI } = require("./keys");
 
+require('./models/user');
+
+mongoose.connect(MONGOURI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false 
+})
+
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongoDB");
+})
+
+mongoose.connection.on('error', (e) => {
+    console.log("error with mongoDB...", e);
+})
 
 
 
 
 app.listen(PORT, () => {
-    console.log("Server is running on ", PORT);
+    console.log("Server is running on", PORT);
 });
 
 
