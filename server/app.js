15bc87ccb381f73084f8d39b0c17fbefaf96dd92
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-10 23:55:44 -0500

    Connect to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,23 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-const customMiddleware = (req, res, next) => {
-    console.log("middleware executed!!");
-    next();
-}
 
 
-
-app.get('/', (req, res) => {
-    console.log("home");
-    res.send("hello world");
-});
-
-app.get('/about', customMiddleware, (req, res) => {
-    console.log("about");
-    res.send("about page");
-});
 
 
 app.listen(PORT, () => {
