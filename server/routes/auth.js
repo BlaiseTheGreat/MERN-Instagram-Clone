9b5c84cc039c12a8eee7a310e7847afe03c02754
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-11 12:40:44 -0500

    Encrypt passwords during registration using bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,37 +2,39 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
 
-router.get('/', (req, res) => {
-    res.send("hello");
-});
 
 router.post('/signup', (req, res) => {
     //console.log(req.body.name);
     const { name, email, password } = req.body;
-    if(!email || !password || !name) {
-        return res.status(422).json({error: "please add all the fields"});
+    if (!email || !password || !name) {
+        return res.status(422).json({ error: "please add all the fields" });
     }
-    User.findOne({email: email})
+    User.findOne({ email: email })
         .then((savedUser) => {
-            if(savedUser) {
-                return res.status(422).json({error: "user already exists with that email"});
+            if (savedUser) {
+                return res.status(422).json({ error: "user already exists with that email" });
             }
-            const user = new User({
-                email,
-                password,
-                name
-            })
+            bcrypt.hash(password, 12)
+                .then(hashedpassWord => {
+                    const user = new User({
+                        email,
+                        password: hashedpassWord,
+                        name
+                    })
 
-            user.save()
-                .then(user=>{
-                    res.json({massage: "saved successfully"});
+                    user.save()
+                        .then(user => {
+                            res.json({ massage: "saved successfully" });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
                 })
-                .catch(err=>{
-                    console.log(err);
-                })
+
         })
-        .catch(err=> {
+        .catch(err => {
             console.log(err);
         })
 })
