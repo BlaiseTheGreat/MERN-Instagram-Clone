290078c5c09fb38302e3a4b2653994797144427d
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-12 14:59:23 -0500

    Create Login and Signup page UI in react

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="Login">
-            <h1>Welcome to the Login page!</h1>
+        <div className="Login mycard">
+            <div className="card auth-card input-field">
+                <h2 className="brand-logo">Instagram</h2>
+                <input
+                    type="text"
+                    placeholder="email"
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Login
+                </button>
+                <h5>
+                    <Link to="/signup">Need to make an account?</Link>
+                </h5>
+            </div>
         </div>
     );
 }
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div className="Signup">
-            <h1>Welcome to the Signup page!</h1>
+        <div className="Signup mycard">
+            <div className="card auth-card input-field">
+                <h2 className="brand-logo">Instagram</h2>
+                <input
+                    type="text"
+                    placeholder="name"
+                />
+                <input
+                    type="text"
+                    placeholder="email"
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                />
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Signup
+                </button>
+                <h5>
+                    <Link to="/login">Already have an account?</Link>
+                </h5>
+            </div>
         </div>
     );
 }
