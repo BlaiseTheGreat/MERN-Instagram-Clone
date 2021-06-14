681393113592d7cdd2e5679dfa009a608e79c7fa
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-14 11:02:56 -0500

    Create static home page UI in React

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,42 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="Home">
-            <h1>Welcome to the Home page!</h1>
+            <div className="card Home-card">
+                <h5>Blaise</h5>
+                <div className="card-image">
+                    <img alt="" src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card Home-card">
+                <h5>Blaise</h5>
+                <div className="card-image">
+                    <img alt="" src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card Home-card">
+                <h5>Blaise</h5>
+                <div className="card-image">
+                    <img alt="" src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is an amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
         </div>
     );
 }
