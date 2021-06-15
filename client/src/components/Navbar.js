8ca818c8e72fc96cde63fb1011d72e08c49a5854
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-15 10:42:59 -0500

    Change navigation links to be dynamic on if authenticated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
 
 const Navbar = () => {
+    const { state, dispatch } = useContext(UserContext);
+    const renderList = () => {
+        if (state) {
+            return [
+                <li><Link to="/profile">Profile</Link></li>,
+                <li><Link to="/create">Create Post</Link></li>
+            ]
+        } else {
+            return [
+                <li><Link to="/login">Log in</Link></li>,
+                <li><Link to="/signup">Sign up</Link></li>
+            ]
+        }
+    }
+
     return (
         <nav>
-            <div className="nav-wrapper white" style={ { color: "black"} }>
-                <Link to="/" className="brand-logo left">MERN Instagram</Link>
+            <div className="nav-wrapper white" style={{ color: "black" }}>
+                <Link to={state? "/" : "/login"} className="brand-logo left">MERN Instagram</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><Link to="/login">Log in</Link></li>
-                    <li><Link to="/signup">Sign up</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/create">Create Post</Link></li>
+                    {renderList()}
                 </ul>
             </div>
         </nav>
