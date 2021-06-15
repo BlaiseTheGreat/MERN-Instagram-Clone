d1ebf68bcffa5e077b4202f1a47fee23aa7509df
Blaise Willoughby <blaise_willoughby@yahoo.com>
2021-06-15 11:15:29 -0500

    Add logout button to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../App';
+import M from 'materialize-css';
 
 const Navbar = () => {
     const { state, dispatch } = useContext(UserContext);
+    const history = useHistory();
     const renderList = () => {
         if (state) {
             return [
                 <li><Link to="/profile">Profile</Link></li>,
-                <li><Link to="/create">Create Post</Link></li>
+                <li><Link to="/create">Create Post</Link></li>,
+                <li>
+                    <button
+                        className="btn waves-effect waves-light #ef5350 red lighten-1"
+                        onClick={() => {
+                            localStorage.clear();
+                            dispatch({type:"CLEAR"});
+                            history.push('/login');
+                            M.toast({ html: "logged out successfully", classes: "#ef5350 red lighten-1" });
+                        }}
+                    >
+                        Logout
+                    </button>
+                </li>
             ]
         } else {
             return [
@@ -21,7 +36,7 @@ const Navbar = () => {
     return (
         <nav>
             <div className="nav-wrapper white" style={{ color: "black" }}>
-                <Link to={state? "/" : "/login"} className="brand-logo left">MERN Instagram</Link>
+                <Link to={state ? "/" : "/login"} className="brand-logo left">MERN Instagram</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     {renderList()}
                 </ul>
